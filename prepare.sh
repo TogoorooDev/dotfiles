256e00e0de04566f0dc0e2a5391f645c1703dd3a
TogoorooDev <hens25252@protonmail.com>
2021-06-27 15:55:15 -0700

    added some package stuff

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
 cd ~
 rsync -a .config/neofetch .config/polybar .config/bspwm .config/alacritty .config/sxhkd .config/zsh-plugins .zshrc .Xresources .xinitrc dotfiles
+
+# Void Package Lists
+xbps-query -l | grep "ii" | cut -d" " -f 2 > void-packages.lst
